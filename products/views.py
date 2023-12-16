3b4d7b1e67a664988af0f14224f2b9079c797e9a
Raimbek Abdykhalyk <raiymbek00.vip@gmail.com>
2023-12-17 01:26:22 +0600

    Initial commit

---
+++ products/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Store',
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    return render(request, 'products/products.html')
